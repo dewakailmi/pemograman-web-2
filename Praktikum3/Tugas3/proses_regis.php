ebbef7e701e4d232789138e85efbf87d3c788d9d
dewakailmi <dewakailmi@gmail.com>
2023-03-14 10:01:31 +0700

    Tugas3

---
+++ Praktikum3/Tugas3/proses_regis.php
@@ -0,0 +1,15 @@
+<?php
+$domisili = ["Jakarta","Bogor","Depok","Tangerang","Bekasi"];
+$program_studi = [
+    "SI"=>"Sistem Informasi",
+    "TI"=>"Teknik Informatika",
+    "BG"=>"Bisnis Digital",
+
+];
+
+$skills = ["HTML", "CSS" , "JavaScript", "PHP", "Python",];
+
+
+
+
+?>
